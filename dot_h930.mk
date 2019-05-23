831361697203036041b2441753ac3f265466ee66
manojpawarsj12 <manojpawarsj.mp11@gmail.com>
2019-05-23 06:11:35 +0000

    h930: dot-ify

--- dot_h930.mk
+++ dot_h930.mk
@@ -1,14 +1,14 @@
 $(call inherit-product, device/lge/h930/full_h930.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device identifiers
 
-PRODUCT_NAME := lineage_h930
+PRODUCT_NAME := dot_h930
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RELEASE_NAME := V30
@@ -24,3 +24,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/190211754b47c:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model
+
+TARGET_BOOT_ANIMATION_RES := 1440
+export DOT_OFFICIAl := true
